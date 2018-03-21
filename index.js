45a1db0372490f196b971c129ed29c4d98cd719f
Sebastian Springer <info@sebastian-springer.com>
2018-03-21 14:04:42 +0100

    module

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const router = require('./movie');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hallo Welt');
 });
 
-app.listen(8080, () => {
-  console.log('Express is listening http://localhost:8080');
+app.use('/movie', router);
+
+const listener = app.listen(8080, () => {
+  console.log(
+    'Express is listening http://localhost:' + listener.address().port,
+  );
 });
